a25083c50a35c18a237d21be28b8a06a773d901b
jonas <tronic2205@gmail.com>
2023-01-16 12:21:57 +0100

    Fixed direction Bug Fixed Bullet Speed

--- MegaMan-Projekt/Assets/scripts/player/DefaultBullet.cs
+++ MegaMan-Projekt/Assets/scripts/player/DefaultBullet.cs
@@ -1,18 +1,28 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DefaultBullet : MonoBehaviour
 {
-
-    [Header("Options")] 
+    [Header("Options")]
     [SerializeField] private float _bulletSpeed = 25f;
-    
+    [SerializeField] public bool isFlipped = false;
+
     private void Update()
     {
         Vector3 position = transform.position;
-        position.x += _bulletSpeed;
+
+        if(isFlipped)
+            position.x += _bulletSpeed * Time.deltaTime;
+        else
+            position.x -= _bulletSpeed * Time.deltaTime;
+
         transform.position = position;
+    }
+
+    private void OnBecameInvisible()
+    {
+        // Destroying the Game Object
+        Destroy(gameObject);
     }
 }
