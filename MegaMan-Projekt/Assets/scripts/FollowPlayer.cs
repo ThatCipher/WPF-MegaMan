82a5f85e3755d207413c9be40108317c86f1c425
ThatCipher <deniz_malik@outlook.de>
2023-02-13 10:55:39 +0100

    Small Updates + Extended map a little + Adjusted Camera to have axis locks

--- MegaMan-Projekt/Assets/scripts/FollowPlayer.cs
+++ MegaMan-Projekt/Assets/scripts/FollowPlayer.cs
@@ -1,20 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
+    [Header("Setup")]
+    [SerializeField] private GameObject cameraFocus;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Constraints")] 
+    [SerializeField] private bool xAxisLock = false;
+    [SerializeField] private bool yAxisLock = false;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 0, -12);
+        MoveCamera();
     }
+
+    void MoveCamera()
+    {
+        Vector3 newPosition = new Vector3();
+        newPosition.x = GetXAxis();
+        newPosition.y = GetYAxis();
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+
+    float GetXAxis()
+    {
+        if (xAxisLock)
+            return transform.position.x;
+        return cameraFocus.transform.position.x;
+    }
+
+    float GetYAxis()
+    {
+        if (yAxisLock)
+            return transform.position.y;
+        return cameraFocus.transform.position.y;
+    }
+    
 }
